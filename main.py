130bc9c67a07acf1ae941cc110ba35c04554df52
Andrew <futureusera@gmail.com>
2019-07-11 04:04:36 +0400

    a

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import io
 import sys
 import os
-
 
 import pyperclip
 import speech_recognition as sr
@@ -14,6 +12,7 @@ from uipyFiles.ui_speech_input import Ui_SpeechInput
 
 with open('google_credentials.json', encoding='utf-8') as inp:
     GOOGLE_CLOUD_SPEECH_CREDENTIALS = inp.read()
+
 LANG_ENG = 'en-US'
 LANG_RUS = 'ru-RU'
 
@@ -68,8 +67,17 @@ class SpeechInput(QWidget):
         self.uiautomat.btn_start.repaint()
 
     def recognize(self, audio, lang):
-        res = self._recognize(audio, language=lang)
-        return res
+        text = None
+        try:
+            text = self.recognizer.recognize_google_cloud(audio, language=lang,
+                                                          credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS)
+            # text = self.recognizer.recognize_google(audio, language=language)
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+        return text
 
     def recognize2(self, content, lang):
         audio = types.RecognitionAudio(content=content.get_flac_data())
@@ -87,19 +95,6 @@ class SpeechInput(QWidget):
             res.append(result.alternatives[0].transcript)
 
         return ' '.join(res)
-
-    def _recognize(self, audio, language=LANG_ENG):
-        text = None
-        try:
-            text = self.recognizer.recognize_google_cloud(audio, language=language,
-                                                          credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS)
-            # text = self.recognizer.recognize_google(audio, language=language)
-        except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
-        except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-        return text
 
 
 if __name__ == '__main__':
